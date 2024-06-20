60b6642a57e67d170bac0246f83bdd57bff60795
realcon02 <alternalegends338@mail.ru>
2024-06-21 01:18:14 +0300

    fix and docs: fix test commands, added doc for bridge_command

--- bot/cogs/general.py
+++ bot/cogs/general.py
@@ -3,13 +3,13 @@ from discord.ext import commands, bridge
 
 
 class General(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot) -> None:
         self.bot = bot
 
     @bridge.bridge_command(name='ping')
     async def ping(self, ctx: bridge.context.BridgeApplicationContext):
         await ctx.respond(f'Pong! {round(ctx.bot.latency * 1000)}ms')
-        await ctx.channel.send(type(ctx))
+        # await ctx.channel.send(type(ctx))
 
 
 def setup(bot) -> None:
--- bot/cogs/template.py
+++ bot/cogs/template.py
@@ -7,19 +7,25 @@ class Template(commands.Cog):
         self.bot = bot
 
     @commands.command(name='test-prefix')
-    async def test_prefix(self, ctx):
-        await ctx.send("Успешный тест!")
+    async def test_prefix(self, ctx: bridge.context.BridgeExtContext):
+        await ctx.reply("Успешный тест!")
         await ctx.send(type(ctx))
 
     @commands.slash_command(name="test-slash")
-    async def test_slash(self, ctx):
-        await ctx.send("Успешный тест!")
-        await ctx.channel.send(type(ctx))
+    async def test_slash(self, ctx: bridge.context.BridgeApplicationContext):
+        await ctx.respond("Успешный тест!")
+        await ctx.send(type(ctx))
 
     @bridge.bridge_command(name='test-bridge')
     async def test_bridge(self, ctx: bridge.context.BridgeApplicationContext):
+        """
+        При вызове через префикс тип ctx - bridge.context.BridgeExtContext
+        При вызове через черту тип ctx - bridge.context.BridgeApplicationContext
+        :param ctx:
+        :return:
+        """
         await ctx.respond("Успешный тест!")
-        await ctx.channel.send(type(ctx))
+        await ctx.send(type(ctx))
 
 
 def setup(bot) -> None:
