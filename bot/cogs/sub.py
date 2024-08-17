4682375bc9d49b9f95e582ba3185bac87f78daa6
realcon02 <alternalegends338@mail.ru>
2024-08-17 20:07:15 +0300

    rename name of Cog to 'sub', add the file structure

---
+++ bot/cogs/sub.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+
+class Subscription(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    subscription = discord.SlashCommandGroup(name='sub')
+
+    @subscription.command(name='help')
+    async def help_subscription(self):
+        pass
+
+    @subscription.command(name='add')
+    async def add_subscription(self):
+        pass
+
+    @subscription.command(name='delete')
+    async def delete_subscription(self):
+        pass
+
+    @subscription.command(name='list')
+    async def list_subscription(self):
+        pass
+
+
+def setup(bot) -> None:
+    bot.add_cog(Subscription(bot))
