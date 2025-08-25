b59098e6053dd83bbbb61b473e8b2eed42911a12
Realcon02 <alternalegends338@mail.ru>
2025-08-25 17:10:42 +0300

    feat(db): add async interface for db with pool connections

---
+++ bot/utils/database.py
@@ -0,0 +1,42 @@
+import asyncpg
+from datetime import datetime
+
+
+class DatabaseManager:
+    """Менеджер базы данных"""
+    def __init__(self):
+        self.pool: asyncpg.Pool | None = None
+
+    async def initialize(self, db_params):
+        self.pool = await asyncpg.create_pool(
+            min_size=1, max_size=8, **db_params
+        )
+        print('[INFO] Database pool initialized')
+
+    async def close(self):
+        if self.pool:
+            await self.pool.close()
+            self.pool = None
+            print('[INFO] Database pool closed')
+
+    async def create_sub(self, target_type: str,
+                         target_id: int,
+                         newest_id_chapter: int) -> None:
+        conn: asyncpg.Connection
+        async with self.pool.acquire() as conn:
+            async with conn.transaction():
+                await conn.execute("""
+                    INSERT INTO
+                    subscriptions(target_type, target_id, newest_id_chapter, created_at)
+                    VALUES ($1, $2, $3, $4)""",
+                    target_type, target_id, newest_id_chapter, datetime.now()
+                )
+
+    async def check_sub_exists(self, target_type: str, target_id: int) -> bool:
+        """Проверка существования подписки"""
+        conn: asyncpg.Connection
+        async with self.pool.acquire() as conn:
+            return await conn.fetchval(
+                "SELECT EXISTS (SELECT 1 FROM subscriptions WHERE target_type = $1 AND target_id = $2)",
+                target_type, target_id
+            )
